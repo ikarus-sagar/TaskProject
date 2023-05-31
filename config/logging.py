f912e011f49da125c96c05e2209f3c09622eeb69
Sagar Gupta <sagar.gupta@ikarus3d.com>
2023-05-31 15:27:21 +0530

    logging

--- config/logging.py
+++ config/logging.py
@@ -2,11 +2,7 @@ import logging,sys
 
 logging.basicConfig(
     level=logging.INFO,
-    format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
-    handlers=[
-        logging.StreamHandler(sys.stdout),
-        logging.FileHandler('app.log')
-    ]
+    format='%(asctime)s - %(name)s - %(levelname)s - %(message)s'
 )
 
 logger = logging.getLogger('my_app')
