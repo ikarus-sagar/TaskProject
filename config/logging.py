ddb070cd79c4419589fcb3de33d49c03f4864155
Sagar Gupta <sagar.gupta@ikarus3d.com>
2023-06-05 15:54:47 +0530

    Linting Correction Some

--- config/logging.py
+++ config/logging.py
@@ -1,4 +1,4 @@
-import logging,sys
+import logging
 
 logging.basicConfig(
     level=logging.INFO,
--- config/secrets_parser.py
+++ config/secrets_parser.py
@@ -1,4 +1,5 @@
-import yaml,pymongo
+import yaml
+import pymongo
 
 with open("config/secrets.yml", 'r') as stream:
     secrets = yaml.safe_load(stream)
@@ -9,14 +10,18 @@ mongodb_username = secrets['mongodb']['username']
 mongodb_password = secrets['mongodb']['password']
 mongodb_database = secrets['mongodb']['database']
 
+
 def get_database():
     client = pymongo.MongoClient(mongodb_database)
     return client["blogging"]
+
 
 def get_blogs_collection():
     db = get_database()
     return db["blogs"]
 
+
 def get_users_collection():
     db = get_database()
     return db["users"]
+
