5dae06493e33f5b84dc4d4b7f9e830175ab4074a
Sagar Gupta <sagar.gupta@ikarus3d.com>
2023-06-07 11:47:22 +0530

    See

--- config/logging.py
+++ config/logging.py
@@ -3,6 +3,5 @@ import yaml
 
 with open('config/logging.yml', 'r') as f:
     logging.config.dictConfig(yaml.safe_load(f.read()))
-    
 logger = logging.getLogger(__name__)
 logger.info('Logging configured')
